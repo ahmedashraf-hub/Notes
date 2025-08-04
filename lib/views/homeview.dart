25898e29bee084f0d4f6187258897b894f4e2a1a
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2025-08-04 03:46:06 +0300

    change theme, create homeView.

---
+++ lib/views/homeview.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:notes/widgets/home_view_body.dart';
+
+class Homeview extends StatelessWidget {
+  const Homeview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: const HomeViewBody());
+  }
+}
