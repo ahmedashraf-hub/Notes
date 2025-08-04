7cb81d968588f9d1ce17479671ed404e027665a8
Ahmed Ashraf <ahmed.bnelashraf@gmail.com>
2025-08-04 04:11:43 +0300

    Create customAppBar.

--- lib/widgets/home_view_body.dart
+++ lib/widgets/home_view_body.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:notes/widgets/custom_app_bar.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [Text('WELCOME YOU IN YOUR NOTES')]);
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(children: [SizedBox(height: 50), CustomAppBar()]),
+    );
   }
 }
